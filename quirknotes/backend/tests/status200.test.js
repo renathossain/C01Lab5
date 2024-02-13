32a375d24b9f0de13515ec199d3751421c2cde50
renathossain <renathossain@gmail.com>
2024-02-13 16:58:19 -0500

    Fixed getAllNotes and wrote postNote getAllNotes 3 test cases

--- quirknotes/backend/tests/status200.test.js
+++ quirknotes/backend/tests/status200.test.js
@@ -1,14 +1,6 @@
-test("1+2=3, empty array is empty", () => {
-  expect(1 + 2).toBe(3);
-  expect([].length).toBe(0);
-});
-
 const SERVER_URL = "http://localhost:4000";
 
-test("/postNote - Post a note", async () => {
-  const title = "NoteTitleTest";
-  const content = "NoteTitleContent";
-
+async function postNoteHelper(title, content) {
   const postNoteRes = await fetch(`${SERVER_URL}/postNote`, {
     method: "POST",
     headers: {
@@ -22,18 +14,64 @@ test("/postNote - Post a note", async () => {
 
   const postNoteBody = await postNoteRes.json();
 
+  return { postNoteRes, postNoteBody };
+}
+
+async function deleteNoteHelper(noteId) {
+  const deleteNoteRes = await fetch(`${SERVER_URL}/deleteNote/${noteId}`, {
+    method: "DELETE",
+  });
+
+  const deleteNoteBody = await deleteNoteRes.json();
+
+  return { deleteNoteRes, deleteNoteBody };
+}
+
+async function getAllNotesHelper() {
+  const getAllNotesRes = await fetch(`${SERVER_URL}/getAllNotes`, {
+    method: "GET"
+  });
+
+  const getAllNotesBody = await getAllNotesRes.json();
+
+  return { getAllNotesRes, getAllNotesBody };
+}
+
+test("/postNote - Post a note", async () => {
+  const { postNoteRes, postNoteBody } = await postNoteHelper(
+    "NoteTitleTest", "NoteTitleContent"
+  );
+
   expect(postNoteRes.status).toBe(200);
   expect(postNoteBody.response).toBe("Note added succesfully.");
+  
+  // Cleanup
+  deleteNoteHelper(postNoteBody.insertedId);
 });
 
 test("/getAllNotes - Return list of zero notes for getAllNotes", async () => {
-  // Code here
-  expect(false).toBe(true);
+  const { getAllNotesRes, getAllNotesBody } = await getAllNotesHelper();
+
+  expect(getAllNotesRes.status).toBe(200);
+  expect(getAllNotesBody.response.length).toBe(0);
 });
 
 test("/getAllNotes - Return list of two notes for getAllNotes", async () => {
-  // Code here
-  expect(false).toBe(true);
+  await postNoteHelper( "NoteTitleTest1", "NoteTitleContent1" );
+  await postNoteHelper( "NoteTitleTest2", "NoteTitleContent2" );
+
+  const { getAllNotesRes, getAllNotesBody } = await getAllNotesHelper();
+
+  expect(getAllNotesRes.status).toBe(200);
+  expect(getAllNotesBody.response.length).toBe(2);
+  expect(getAllNotesBody.response[0].title).toBe("NoteTitleTest1");
+  expect(getAllNotesBody.response[1].title).toBe("NoteTitleTest2");
+  expect(getAllNotesBody.response[0].content).toBe("NoteTitleContent1");
+  expect(getAllNotesBody.response[1].content).toBe("NoteTitleContent2");
+
+  // Cleanup
+  await deleteNoteHelper(getAllNotesBody.response[0]._id);
+  await deleteNoteHelper(getAllNotesBody.response[1]._id);
 });
 
 test("/deleteNote - Delete a note", async () => {
